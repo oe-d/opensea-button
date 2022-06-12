6d560f7656f5a3991caa185ffe8f6510c47398cc
oe-d <70195042+oe-d@users.noreply.github.com>
2022-06-12 12:35:00 +0200

    Add tooltip

--- opensea-button/opensea-button.js
+++ opensea-button/opensea-button.js
@@ -1,4 +1,4 @@
-// OpenSea Button 1.0.0
+// OpenSea Button 1.1.0
 // https://github.com/oe-d/opensea-button
 
 page = '';
@@ -34,26 +34,59 @@ function get_os_url() {
     return os_url + href.substr(href.search('0x'), 42);
 }
 
+function create_elements(btn_container) {
+    btn_1 = document.createElement('span');
+    btn_2 = document.createElement('a');
+    btn_3 = document.createElement('img');
+
+    btn_2.appendChild(btn_3);
+    btn_1.appendChild(btn_2);
+    btn_container.appendChild(btn_1);
+
+    tt_1 = document.createElement('div');
+    tt_2 = document.createElement('div');
+    tt_3 = document.createElement('div');
+
+    tt_1.appendChild(tt_2);
+    tt_1.appendChild(tt_3);
+    document.body.appendChild(tt_1);
+
+    btn_2.setAttribute('class', 'ml-1 btn btn-sm btn-icon btn-soft-primary rounded-circle');
+    btn_2.setAttribute('href', get_os_url());
+
+    btn_2.addEventListener('mouseover', function () {
+        tt_1.setAttribute('class', 'tooltip fade bs-tooltip-top show');
+    });
+
+    btn_2.addEventListener('mouseleave', function () {
+        tt_1.setAttribute('class', 'tooltip fade bs-tooltip-top');
+    });
+
+    btn_3.setAttribute('class', 'img-fluid rounded-circle');
+    btn_3.setAttribute('src', chrome.runtime.getURL('opensea-logo.svg'));
+
+    btn_2_rect = btn_2.getBoundingClientRect();
+    btn_2_center = btn_2_rect.left + (btn_2_rect.right - btn_2_rect.left) / 2;
+
+    tt_3.setAttribute('class', 'tooltip-inner');
+    tt_3.textContent = 'View on OpenSea';
+
+    tt_1.setAttribute('class', 'tooltip bs-tooltip-top');
+    tt_1_rect = tt_1.getBoundingClientRect();
+    tt_1_x = Math.round(btn_2_center - ((tt_1_rect.right - tt_1_rect.left) / 2));
+    tt_1_y = Math.round(btn_2_rect.top - (tt_1_rect.bottom - tt_1_rect.top));
+    tt_1.setAttribute('style', 'position: absolute; transform: translate3d(' + tt_1_x + 'px, ' + tt_1_y + 'px, 0px); top: 0px; left: 0px; will-change: transform;');
+    tt_1.setAttribute('x-placement', 'top');
+
+    tt_2.setAttribute('class', 'arrow');
+    tt_2_rect = tt_2.getBoundingClientRect();
+    tt_2_x = (tt_1_rect.right - tt_1_rect.left) / 2 - ((tt_2_rect.right - tt_2_rect.left) / 2);
+    tt_2.setAttribute('style', 'left: ' + tt_2_x + 'px;');
+}
+
 if (page.length > 0) {
-    os_url = get_os_url();
-    os_logo = chrome.runtime.getURL('opensea-logo.svg');
-
-    el_1 = document.createElement('span');
-    el_2 = document.createElement('a');
-    el_3 = document.createElement('img');
-
-    el_2.setAttribute('class', 'ml-1 btn btn-sm btn-icon btn-soft-primary rounded-circle');
-    el_2.setAttribute('href', os_url);
-
-    el_3.setAttribute('class', 'img-fluid rounded-circle');
-    el_3.setAttribute('src', os_logo);
-
-    el_2.appendChild(el_3);
-    el_1.appendChild(el_2);
-
     nodes = document.getElementsByClassName('mb-3 mb-lg-0')[0].firstChild.nextSibling.childNodes;
     container = nodes[page == 'token' ? 3 : 5];
-    container.appendChild(el_1);
     bc_node = container.getElementsByClassName('mr-1 ml-2');
 
     if (bc_node[0]) {
@@ -70,4 +103,6 @@ if (page.length > 0) {
             }
         }
     }
+
+    create_elements(container);
 }
